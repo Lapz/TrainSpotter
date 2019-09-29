357b7addd53d4d2def9f7228bf68243972020cf9
Lenard Pratt <l3np27@gmail.com>
2019-09-29 11:02:53 +0100

    Fixed static assets

--- helpers/ResultCard.ts
+++ helpers/ResultCard.ts
@@ -11,19 +11,19 @@ import { createStationQueryUrl } from "./SearchBar"
 export const getRailLogo = (railType: string) => {
   switch (railType) {
     case "tube":
-      return "static/tfl/tube.svg"
+      return "/static/tfl/tube.svg"
 
     case "national-rail":
-      return "static/tfl/national-rail.svg"
+      return "/static/tfl/national-rail.svg"
 
     case "tflrail":
-      return "static/tfl/tflrail.svg"
+      return "/static/tfl/tflrail.svg"
     case "bus":
-      return "static/tfl/bus.svg"
+      return "/static/tfl/bus.svg"
     case "overground":
-      return "static/tfl/overground.svg"
+      return "/static/tfl/overground.svg"
     case "dlr":
-      return "static/tfl/dlr.svg"
+      return "/static/tfl/dlr.svg"
 
     default:
       return `unknown rail type: ${railType}`
