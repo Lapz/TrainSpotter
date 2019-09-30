451addc87e19064aeb3a5edaea32bc757a14979d
Lenard Pratt <l3np27@gmail.com>
2019-09-30 19:28:30 +0100

    Full add mobx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,47 +1,56 @@
-import React from "react"
-import App from "next/app"
-import Head from "next/head"
-import { ThemeProvider } from "@material-ui/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import theme from "../theme/theme"
+import { ThemeProvider } from "@material-ui/styles"
 import firebase from "firebase/app"
+import { Provider } from "mobx-react"
+import { getSnapshot } from "mobx-state-tree"
+import { NextComponentType, NextPageContext } from "next"
+import App, { AppInitialProps } from "next/app"
+import Head from "next/head"
+import { Router } from "next/router"
+import React from "react"
 import { FIREBASE_CONFIG } from "../config/google"
-import { IStore, initializeStore } from "../stores"
-
+import { initializeStore, IStore } from "../stores"
+import theme from "../theme/theme"
 interface IOwnProps {
   isServer: boolean
   initialState: IStore
 }
 
-export default class MyApp extends App {
+export default class MyApp extends App<IOwnProps> {
+  public static async getInitialProps({
+    Component,
+    ctx
+  }: {
+    Component: NextComponentType
+    ctx: NextPageContext
+  }) {
+    //
+    // Use getInitialProps as a step in the lifecycle when
+    // we can initialize our store
+    //
+    const isServer = typeof window === "undefined"
+    const store = initializeStore(isServer)
+
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return {
+      initialState: getSnapshot(store),
+      isServer,
+      pageProps
+    }
+  }
+
   private store: IStore
 
-  // constructor(props) {
-  //   super(props)
-  //   this.store = initializeStore(props.isServer, props.initialState) as IStore
-  // }
+  constructor(props: any) {
+    super(props)
+    this.store = initializeStore(props.isServer, props.initialState) as IStore
+  }
 
-  // public static async getInitialProps({ Component, router, ctx }) {
-  //   //
-  //   // Use getInitialProps as a step in the lifecycle when
-  //   // we can initialize our store
-  //   //
-  //   const isServer = typeof window === "undefined"
-  //   const store = initializeStore(isServer)
-
-  //   let pageProps = {}
-  //   if (Component.getInitialProps) {
-  //     pageProps = await Component.getInitialProps(ctx)
-  //   }
-
-  //   return {
-  //     initialState: getSnapshot(store),
-  //     isServer,
-  //     pageProps
-  //   }
-  // }
-
-  componentDidMount() {
+  public componentDidMount() {
     firebase.initializeApp(FIREBASE_CONFIG)
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side")
@@ -51,7 +60,7 @@ export default class MyApp extends App {
     }
   }
 
-  render() {
+  public render() {
     const { Component, pageProps } = this.props
 
     return (
@@ -62,7 +71,9 @@ export default class MyApp extends App {
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
-          <Component {...pageProps} />
+          <Provider store={this.store}>
+            <Component {...pageProps} />
+          </Provider>
         </ThemeProvider>
       </React.Fragment>
     )
--- stores/index.ts
+++ stores/index.ts
@@ -6,15 +6,13 @@ import {
   types
 } from "mobx-state-tree"
 
+import { User } from "./user"
+
 let store: IStore = null as any
 
-const Store = types
-  .model({
-    user: {}
-  })
-  .actions((self) => {
-    return {}
-  })
+const Store = types.model({
+  user: types.maybe(User)
+})
 
 export type IStore = Instance<typeof Store>
 export type IStoreSnapshotIn = SnapshotIn<typeof Store>
@@ -22,12 +20,14 @@ export type IStoreSnapshotOut = SnapshotOut<typeof Store>
 
 export const initializeStore = (isServer: boolean, snapshot = null) => {
   if (isServer) {
-    store = Store.create({ user: null })
+    store = Store.create({ user: undefined })
   }
   if ((store as any) === null) {
-    store = Store.create({ user: null })
+    store = Store.create({ user: undefined })
   }
   if (snapshot) {
     applySnapshot(store, snapshot)
   }
+
+  return store
 }
